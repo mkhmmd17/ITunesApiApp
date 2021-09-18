ef55d70d2f8a9a387acc1815c2ce9fa54b267dda
mkhmmd17 <maxmelhi95@gmail.com>
2021-09-18 10:36:18 +0300

    Initial Commit

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by muslim on 18.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
